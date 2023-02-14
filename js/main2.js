a33ae8dbbf9af34c17c977d0e1570669873b4d6c
Emanuel Evangelista <97486394+ElBigas@users.noreply.github.com>
2023-02-13 22:52:07 -0300

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -1,16 +1,24 @@
+/**
+ * Essa função é usada para iniciar o jogo, mas antes de iniciar, 
+ * verifica se o player selecionou um nível. Isso é feito 
+ * verificando se o valor do elemento com ID "nivel" é vazio. Se 
+ * o valor for vazio, uma mensagem de alerta é exibida para o 
+ * player e a função retorna "false". Se o valor não for vazio, 
+ * o player é redirecionado para a página "index.html" com o 
+ * valor do nível adicionado como parâmetro na URL.
+ * 
+ */
+
 function iniciarJogo() {
     
-    // Obtém o valor do elemento com o ID 'nivel'
     let nivel = document.getElementById('nivel').value;
   
-    // Verifica se o valor é vazio
     if (nivel === '') {
       console.log(nivel);
-      alert('Escolha um nível'); // Exibe um alerta para o usuário
-      return false; // Retorna falso para interromper o fluxo da função
+      alert('Escolha um nível'); 
+      return false; 
     } 
   
-    // Se o valor não for vazio, redireciona o usuário para 'index.html'
     window.location.href = 'index.html?' + nivel;
   }
   
